da2b259e815de99d5c292cdb6b98a1d5854334c0
cx943b <cx943b@gmail.com>
2024-01-26 14:29:10 +0900

    create: AddWorkerView,AddWorkerViewModel

---
+++ WorkTimeTable/ViewModels/AddWorkerViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkTimeTable.Infrastructure;
+using WorkTimeTable.Infrastructure.Models;
+
+namespace WorkTimeTable.ViewModels
+{
+    internal partial class AddWorkerViewModel : ObservableValidator
+    {
+        [ObservableProperty]
+        [Required]
+        string _Name;
+
+        [ObservableProperty]
+        DayOfWeekFlag _FixedWorkWeeks;
+
+        public AddWorkerViewModel(ILogger<AddWorkerViewModel> logger)
+        {
+            
+        }
+    }
+}
