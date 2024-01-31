fbc450b10972f1c1d4a8206c6ad3288ed7ff94d3
Gyu Jin Lee <cx943b@gmail.com>
2024-02-01 02:13:00 +0900

    ing: DayofOfWeekFlag to string ing: Ui AddWorkerView

--- WorkTimeTable.Infrastructure/DayOfWeekFlag.cs
+++ WorkTimeTable.Infrastructure/DayOfWeekFlag.cs
@@ -1,15 +1,26 @@
-﻿namespace WorkTimeTable.Infrastructure
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkTimeTable.Infrastructure
 {
     [Flags]
     public enum DayOfWeekFlag
     {
         None =      0x0000_0000,
+
+        [Description("Sun")]
         Sunday =    0x0000_0010,
+        [Description("Mon")]
         Monday =    0x0000_0100,
+        [Description("Tue")]
         Tuesday =   0x0000_1000,
+        [Description("Wed")]
         Wednesday = 0x0001_0000,
+        [Description("Thu")]
         Thursday =  0x0010_0000,
+        [Description("Fri")]
         Friday =    0x0100_0000,
+        [Description("Sat")]
         Saturday =  0x1000_0000
     }
 
