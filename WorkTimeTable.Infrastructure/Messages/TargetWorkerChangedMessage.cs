9b92ef6b0aa7a928e1d0944f15ddd06998188be5
cx943b <cx943b@gmail.com>
2024-06-17 13:49:51 +0900

    작업자 관리 및 시간 필터링 로직 개선
    TargetWorkerInfoChangedMessage 클래스 추가 및 EntireWorkTimeViewModel, WorkerInfoViewModel 클래스에서 중요한 변경사항이 있었습니다. System.Windows.Data 네임스페이스가 추가되어 데이터 바인딩 기능이 강화되었으며, 작업자 관리 및 작업 시간 필터링 로직이 개선되었습니다. EntireWorkTimeViewModel에서는 _workers 필드가 제거되고, IWorkerManageService 타입의 _workerMgrSvc 필드가 추가되어 작업자 관리 서비스 의존성이 주입되었습니다. 또한, FilteredWorkTimesList 속성 대신 ObservableCollection<FilteredWorkTimesModel> 타입의 _lstFilteredWorkTimes 및 CollectionViewSource 타입의 _cvsFilteredWorkTimes가 추가되어, 필터링된 작업 시간 관리가 개선되었습니다. WorkerInfoViewModel에서는 OnColorNameChanged 메서드가 수정되어, 색상 이름 변경 시 TargetWorkerInfoChangedMessage 메시지를 전송하도록 변경되었습니다. 이러한 변경사항은 데이터 바인딩과 MVVM 패턴을 더욱 효율적으로 활용하여 UI와 비즈니스 로직 간의 결합도를 낮추고, 유지보수성을 향상시키는 것을 목표로 합니다.

--- WorkTimeTable.Infrastructure/Messages/TargetWorkerChangedMessage.cs
+++ WorkTimeTable.Infrastructure/Messages/TargetWorkerChangedMessage.cs
@@ -3,6 +3,16 @@ using WorkTimeTable.Infrastructure.Interfaces;
 
 namespace WorkTimeTable.Infrastructure.Messages
 {
+    public class TargetWorkerInfoChangedMessage : ValueChangedMessage<IWorker>
+    {
+        public TargetWorkerInfoChangedMessage(IWorker worker) : base(worker)
+        {
+            if(worker == null)
+            {
+                throw new System.ArgumentNullException(nameof(worker));
+            }
+        }
+    }
     public class TargetWorkerChangedMessage : ValueChangedMessage<IWorker?>
     {
         public TargetWorkerChangedMessage(IWorker? worker) : base(worker)
